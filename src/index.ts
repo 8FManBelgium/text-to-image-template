95b9fa9766a329ad81ae228d0e6182c8f37dd300
8FManBelgium <bausmans@gmail.com>
2026-01-16 11:41:46 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(request, env) {
 		const inputs = {
-			prompt: "a frog wearing sunglases",
+			prompt: "a panda cooking noodels realistic image",
 		};
 
 		const response = await env.AI.run(
